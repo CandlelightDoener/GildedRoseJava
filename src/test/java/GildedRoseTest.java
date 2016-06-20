4bb3215525ded2a61aecc7ebccaf4b7c5d4f106a
Daniel Zappold <daniel.zappold@improuv.com>
2016-06-20 11:11:43 +0200

    property: quality can never be negative with fixed item

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,27 @@
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.generator.InRange;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 
+@RunWith(JUnitQuickcheck.class)
 public class GildedRoseTest {
 
 	@Test
 	public void testTheTruth() {
 		assertTrue(true);
+	}
+
+	@Property
+	public void qualityIsNeverNegative(@InRange(minInt=-10, maxInt=10) int sellIn, @InRange(minInt=0, maxInt= 10) int quality){
+		Item item = new Item("hhdwgdzgw", sellIn, quality);
+		GildedRose.with(item).updateQuality();
+		assertThat(item.getQuality(), not(lessThan(0)));
 	}
 }
